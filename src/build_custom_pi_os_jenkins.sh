e7cf7562a9dc8ae0b6cb8ce6b2526722ace8e304
Guy Sheffer <guysoft@gmail.com>
2024-01-12 13:44:40 +0200

    Build to a cache folder so we can do multiple builds

--- src/build_custom_pi_os_jenkins.sh
+++ src/build_custom_pi_os_jenkins.sh
@@ -1,4 +1,6 @@
-#!/bin/bash -x
+#!/bin/bash
+set -x
+set -e
 
 CUSTOM_PI_OS_WORKSPACE=workspace
 
@@ -16,6 +18,9 @@ pushd "${DISTRO_PATH}"
     DIRNAME=$(realpath "./src/${CUSTOM_PI_OS_WORKSPACE}")
     DATE=$(date --rfc-3339=date)
     NEW_ZIP_NAME=${DIRNAME}/${DATE}_${BASENAME}
+    TEMP_COPY_DIR=/mnt/sda1/unofficialpi/upload_cache
+    TEMP_COPY_DEST="${TEMP_COPY_DIR}/custompios/${BUILD_NUMBER}"
+    mkdir -p "${TEMP_COPY_DEST}"
     
     RPI_JSON_PATH_BASE=rpi-imager-snipplet.json
     FULL_RPI_JSON_PATH="${DIRNAME}/${RPI_JSON_PATH_BASE}"
@@ -25,14 +30,14 @@ pushd "${DISTRO_PATH}"
     shopt -s nullglob
     for export_file in "${DIRNAME}"/*.tar.gz
     do
-        cp "${export_file}" "${WORKSPACE}"/$(date --rfc-3339=date)_$(basename ${export_file}) || true
+        cp "${export_file}" "${TEMP_COPY_DEST}"/$(date --rfc-3339=date)_$(basename ${export_file}) || true
     done
     
     mv "${ZIPNAME}" "${NEW_ZIP_NAME}"
     
-    mv "${NEW_ZIP_NAME}" "${WORKSPACE}"/`basename ${NEW_ZIP_NAME}`
+    mv "${NEW_ZIP_NAME}" "${TEMP_COPY_DEST}"/`basename ${NEW_ZIP_NAME}`
     
-    pushd "${WORKSPACE}"
+    pushd "${TEMP_COPY_DEST}"
         md5sum `basename ${NEW_ZIP_NAME}` > `basename ${NEW_ZIP_NAME}`.md5
         echo mv "${FULL_RPI_JSON_PATH}" "${NEW_RPI_JSON_PATH_BASE}"
         ls ${FULL_RPI_JSON_PATH}
@@ -44,9 +49,9 @@ popd
 
 
 # Add files to  ftp upload publish script
-pushd "${WORKSPACE}"    
-    unset IFS; a=("$PWD"/*); printf '%s\0' "${a[@]}" > "${WORKSPACE}/files"
+pushd "${TEMP_COPY_DEST}"
+    unset IFS; a=("$PWD"/*); printf '%s\0' "${a[@]}" > "${TEMP_COPY_DEST}/files"
     
-    echo  FileNamesPath=${WORKSPACE}/files > "${WORKSPACE}/upload_this"
-    echo "Directory=${FTP_PUBLISH_PATH}" >> "${WORKSPACE}/upload_this"
+    echo  FileNamesPath=${TEMP_COPY_DEST}/files > "${TEMP_COPY_DEST}/upload_this"
+    echo "Directory=${FTP_PUBLISH_PATH}" >> "${TEMP_COPY_DEST}/upload_this"
 popd
